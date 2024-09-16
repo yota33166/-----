492815f73a301365ee1ab61e6817b5d0a59a3ccc
Yota Nagai <y.naga.23.7.1110@4nonome.com>
2024-09-16 23:17:20 +0900

    add: 音声を同時に再生しないようにする

--- play_sound.py
+++ play_sound.py
@@ -6,6 +6,9 @@ providing_wav = "sound\\metan_providing.wav"
 
 pygame.mixer.init()
 
+# スレッド排他制御用のロックを作成
+lock = threading.Lock()
+
 def play_sound(providing_num:int):
     "引数で指定した番号を呼び出す音声を再生"
     sound_files = [num_wav, f"sound\\metan_{str(providing_num)}.wav", providing_wav]
@@ -17,8 +20,14 @@ def play_sound(providing_num:int):
 
 # 音声再生を別スレッドで開始
 def play_sound_thread(providing_num:int):
-    thread = threading.Thread(target=play_sound, args=(providing_num,))
+    """音声再生を別スレッドで実行し、同時に並列実行されないように制御"""
+    def sound_with_lock():
+        with lock:
+            play_sound(providing_num)
+    
+    thread = threading.Thread(target=sound_with_lock)
     thread.start()
 
 if __name__ == "__main__":
     play_sound_thread("1")
+    play_sound_thread("2")
